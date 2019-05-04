c774fdd7ab8a761110a6502564fd5cba98a2bf68
Wanninger Wolfgang <wanningerwolfgang@Wanningers-MacBook-Air.local>
2019-05-04 18:03:59 +0800

    finish class 4 week 4

---
+++ 4_class/dbs/sql_create_join-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Member (
+	user_id INTEGER,
+	course_id INTEGER,
+	role INTEGER,
+	PRIMARY KEY (user_id, course_id)
+)
---
+++ 4_class/dbs/sql_populate-user-course.sql
@@ -0,0 +1,7 @@
+INSERT INTO User (name, email) VALUES ('Jane', 'jane@tsgui.org');
+INSERT INTO User (name, email) VALUES ('Ed', 'ed@tsgui.org');
+INSERT INTO User (name, email) VALUES ('Sue', 'sue@tsgui.org');
+
+INSERT INTO Course (title) VALUES ('Python');
+INSERT INTO Course (title) VALUES ('SQL');
+INSERT INTO Course (title) VALUES ('PHP');
